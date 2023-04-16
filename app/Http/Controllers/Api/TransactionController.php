61746ae9dddc91b6fe9bc4cba38a7355c6e65d1a
james19999 <komlanahiakpor23@gmail.com>
2023-04-16 11:32:15 +0200

    new review

--- app/Http/Controllers/Api/TransactionController.php
+++ app/Http/Controllers/Api/TransactionController.php
@@ -54,14 +54,17 @@ class TransactionController extends Controller
                     'created'        =>Carbon::now(),
                 ]);
 
-                // $company_email=Companies::findOrfail($usecartid->company_id);
-                // $email=$company_email->email;
-                // $transaction_cartcode=$transactions->cartcode;
-                // $transaction_title=$transactions->title;
-                // $transaction_code_tansaction=$transactions->code_tansaction;
-                // $user_name=$user->name;
+                $company_email=Companies::findOrfail($usecartid->company_id);
 
-                // Mail::to($email)->send(new NewTransaction($transaction_code_tansaction,$user_name,$transaction_cartcode, $transaction_title));
+                $username=CompanieCostumer::findOrfail($user);
+                $user_name=$username->name;
+                $email=$company_email->email;
+
+                $transaction_cartcode=$transactions->cartcode;
+                $transaction_title=$transactions->title;
+                $transaction_code_tansaction=$transactions->code_tansaction;
+
+                Mail::to($email)->send(new NewTransaction($transaction_code_tansaction,$user_name,$transaction_cartcode, $transaction_title));
 
                return Helpers::response("success",true);
              }
